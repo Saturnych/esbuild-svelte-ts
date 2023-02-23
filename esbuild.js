085bafc7295d0ac0870e770d8473911bead502d0
Denis <saturnych@gmail.com>
2023-02-23 13:59:56 +0300

    Initial commit

---
+++ esbuild.js
@@ -0,0 +1,55 @@
+import { build, context } from 'esbuild';
+import svelte from 'esbuild-svelte';
+import preprocess from 'svelte-preprocess';
+import rm from './env/rm.js';
+import log from './env/log.js';
+import meta from './env/meta.js';
+
+const DEV = process.argv.includes('--dev');
+
+const serveOptions = {
+    servedir: 'public'
+};
+
+const svelteOptions = {
+    compileOptions: {
+        dev: DEV,
+        css: false,
+        immutable: true
+    },
+    preprocess: [
+        preprocess({
+            sourceMap: DEV,
+            typescript: true,
+        }),
+    ]
+};
+
+const buildOptions = {
+    bundle: true,
+    minify: !DEV,
+    sourcemap: DEV,
+    entryPoints: ['src/app.ts'],
+    outdir: 'public/build',
+    format: 'esm',
+    loader: { '.svg': 'text' },
+    plugins: [svelte(svelteOptions), log],
+    inject: DEV ? ['./env/lr.js'] : [],
+    legalComments: "none",
+    logLevel: 'info',
+    metafile: !DEV
+};
+
+await rm('public/build');
+
+if (DEV) {
+    const ctx = await context(buildOptions);
+
+    await ctx.watch();
+    await ctx.serve(serveOptions);
+
+    process.on('SIGTERM', ctx.dispose);
+    process.on("exit", ctx.dispose);
+} else {
+    await meta(await build(buildOptions));
+}
